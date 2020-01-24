b53763028bce8a21011e28cf1752bbe1fd9a4a52
Royer Corentin <proyer.corentin@gmail.com>
2020-01-24 14:38:40 +0100

    non

--- js/reservation.js
+++ js/reservation.js
@@ -12,12 +12,159 @@ class Reservation {
         this.closeResa = document.getElementById("close_resa");
         this.annulationBouton.style.display = "none";
         this.voletResa = document.getElementById("reservation");
-        this.inputNom.value = localStorage.getItem("inputName");
-        this.inputPrenom.value = localStorage.getItem("inputFirstName");
-        this.initMouse();
+        this.divInfos = document.getElementById("infos_resa");
+        this.divConfirmation = document.getElementById("confirmation_resa");
+        this.resaEnCours = document.getElementById("reservation_en_cours");
+        this.boutonConfirmation = document.getElementById("bouton_confirmation");
+        window.onload = this.resaCheck();
+        this.storageResa();
+        this.startResa();
+        this.initCanvas();
+    }
+    startResa() {
+        this.boutonResa.addEventListener('click', () => {
+            // Check des inputs
+            if (this.inputNom.value == "") {
+                alert("Vous avez oublié de mettre votre nom");
+            } else if (this.inputPrenom.value == "") {
+                alert("Vous avez oublié votre Prénom");
+            } else {
+                this.divInfos.style.display = "none";
+                this.divConfirmation.style.display = "block";
+                this.boutonResa.style.display = "none";
+                this.confirmationResa();
+            }
+        });
+        this.annulationBouton.addEventListener('click', () => {
+            this.annulationResa();
+            alert("Réservation annulée!");
+        });
+        this.closeResa.addEventListener("click", () => {
+            this.voletResa.style.width = "0";
+            this.voletResa.style.opacity = "0";
+            this.voletResa.style.right = "0";
+            this.voletResa.style.height = "0";
+
+        });
+        // Remplissage des inputs
+    }
+    confirmationResa() {
+        this.boutonConfirmation.addEventListener("click", () => {
+            if (this.isSigned === true) {
+                sessionStorage.removeItem("recupEndTimer");
+                this.divConfirmation.style.display = "none";
+                this.resaEnCours.style.display = "flex";
+                this.recupData();
+                this.startTimer();
+                this.annulationBouton.style.display = "block";
+            } else {
+                alert("Vous n'avez pas signé !");
+            }
+        });
+    }
+    recupData() {
+        this.recupStation = document.getElementById('nom_station').textContent;
+        sessionStorage.setItem("sessionStation", this.recupStation);
+        this.recapTexte = document.getElementById('recap_texte');
+        this.recapPrenom = localStorage.getItem('inputFirstName');
+        this.recapNom = localStorage.getItem('inputName');
+        this.recapTexte.innerText = " Vous avez reservé un vélo a la Station : " + this.recupStation + " " + "Au nom de :" + " " + this.recapPrenom + " " + this.recapNom;
+    }
+    startTimer() {
+        this.start = (new Date()).getTime();
+        this.divTimer = document.getElementById("recap_timer");
+        sessionStorage.setItem("start", this.start);
+        this.intervalTimer = setInterval(() => {
+            this.now = (new Date()).getTime();
+            // console.log(now-start);
+            this.time = 1200 - Math.floor((this.now - this.start) / 1000);
+            this.testTimer = Math.floor(this.time / 60) + ":" + (this.time % 60);
+            this.endTimer = this.start + 1200000;
+            sessionStorage.setItem('recupEndTimer', this.endTimer);
+            if (this.now > this.endTimer) {
+                this.annulationResa();
+                this.resaTimeOut();
+            }
+            console.log(this.testTimer);
+            this.divTimer.innerHTML = "Temps restant :" + "<br/>" + this.testTimer;
+        }, 1000);
 
     }
-    initMouse() {
+    resaCheck() {
+        this.start = (new Date()).getTime();
+        this.divTimer = document.getElementById("recap_timer");
+        if (sessionStorage.getItem("start")) {
+            this.recupStation = document.getElementById('nom_station').textContent;
+            this.recapTexte = document.getElementById('recap_texte');
+            this.recapPrenom = localStorage.getItem('inputFirstName');
+            this.recapNom = localStorage.getItem('inputName');
+            this.sessionStation = sessionStorage.getItem("sessionStation");
+            this.recapTexte.innerText = " Vous avez reservé un vélo a la Station : " + this.sessionStation + " " + "Au nom de :" + " " + this.recapPrenom + " " + this.recapNom;
+            this.annulationBouton.style.display = "block";
+            this.start = sessionStorage.getItem("start");
+            this.divInfos.style.display = "none";
+            this.divConfirmation.style.display = "none";
+            this.boutonResa.style.display = "none";
+            this.resaEnCours.style.display = "flex";
+            console.log(this.start);
+            this.intervalTimer = setInterval(() => {
+                this.now = (new Date()).getTime();
+                // console.log(now-start);
+                this.time = 1200 - Math.floor((this.now - this.start) / 1000);
+                this.testTimer = Math.floor(this.time / 60) + ":" + (this.time % 60);
+                this.endTimer = sessionStorage.getItem('recupEndTimer');
+                console.log(this.now);
+                console.log(this.endTimer);
+                if (this.now > this.endTimer) {
+                    this.annulationResa();
+                    this.resaTimeOut();
+                }
+                console.log(this.testTimer);
+                this.divTimer.innerHTML = "Temps restant :" + "<br/>" + this.testTimer;
+            }, 1000);
+        }
+
+    }
+
+    resaTimeOut() {
+        alert("Votre réservation a expiré");
+        this.divTimer.innerText = "";
+        this.recapText.innerText = " Votre reservation a expiré.";
+        this.annulationBouton.style.display = "none";
+        this.resaEnCours.style.display = "none";
+        this.divConfirmation.style.display = "none";
+        this.divInfos.style.display = "block";
+        this.boutonResa.style.display = "block";
+    }
+
+    annulationResa() {
+        clearInterval(this.intervalTimer);
+        sessionStorage.removeItem("start");
+        this.divTimer.innerText = " ";
+        this.recapText.innerText = "Vous n'avez pas de réservation pour le moment.";
+        this.annulationBouton.style.display = "none";
+        this.resaEnCours.style.display = "none";
+        this.divConfirmation.style.display = "none";
+        this.divInfos.style.display = "block";
+        this.boutonResa.style.display = "block";
+
+
+    }
+    storageResa() {
+        this.inputNom.value = localStorage.getItem("inputName");
+        this.inputPrenom.value = localStorage.getItem("inputFirstName");
+        this.boutonResa.addEventListener('click', () => {
+            localStorage.setItem("inputName", this.inputNom.value);
+            console.log(this.inputNom.value);
+            localStorage.setItem("inputFirstName", this.inputPrenom.value);
+            console.log(this.inputPrenom.value);
+            this.storedName = localStorage.getItem('inputName');
+            console.log(this.storedName);
+            this.storedFirstName = localStorage.getItem('inputFirstName');
+            console.log(this.storedFirstName);
+        });
+    }
+    initCanvas() {
         this.canvas = document.getElementById("canvas_signature");
         this.ctx = this.canvas.getContext("2d");
         this.ctx.fillStyle = "black";
@@ -68,122 +215,8 @@ class Reservation {
             this.draw = false;
             this.ctx.closePath();
         });
-
-
-        window.onload = this.resaCheck();
-        this.boutonResa.addEventListener('click', () => {
-            // Check des inputs
-            if (this.inputNom.value == "") {
-                alert("Vous avez oublié de mettre votre nom");
-            } else if (this.inputPrenom.value == "") {
-                alert("Vous avez oublié votre Prénom");
-            } else {
-                sessionStorage.removeItem("recupEndTimer");
-                this.recupData();
-                this.startTimer();
-                this.annulationBouton.style.display = "block";
-            }
-        });
-        this.annulationBouton.addEventListener('click', () => {
-            this.annulationResa();
-            alert("Réservation annulée!");
-        })
-        this.closeResa.addEventListener("click", () => {
-            this.voletResa.style.width = "0";
-            this.voletResa.style.opacity = "0";
-            this.voletResa.style.right = "0";
-            this.voletResa.style.height = "0";
-
-        })
-        // Remplissage des inputs
-    }
-    storageResa() {
-        this.boutonResa.addEventListener('click', () => {
-            localStorage.setItem("inputName", this.inputNom.value);
-            console.log(this.inputNom.value);
-            localStorage.setItem("inputFirstName", this.inputPrenom.value);
-            console.log(this.inputPrenom.value);
-            this.storedName = localStorage.getItem('inputName');
-            console.log(this.storedName);
-            this.storedFirstName = localStorage.getItem('inputFirstName');
-            console.log(this.storedFirstName);
-        })
-    }
-    recupData() {
-        this.recupStation = document.getElementById('nom_station').textContent;
-        sessionStorage.setItem("sessionStation", this.recupStation);
-        this.recapTexte = document.getElementById('recap_texte');
-        this.recapPrenom = localStorage.getItem('inputFirstName');
-        this.recapNom = localStorage.getItem('inputName');
-        this.recapTexte.innerText = " Vous avez reservé un vélo a la Station : " + this.recupStation + " " + "Au nom de :" + " " + this.recapPrenom + " " + this.recapNom;
-    }
-    startTimer() {
-        this.start = (new Date()).getTime();
-        this.divTimer = document.getElementById("recap_timer");
-        sessionStorage.setItem("start", this.start);
-        this.intervalTimer = setInterval(() => {
-            this.now = (new Date()).getTime();
-            // console.log(now-start);
-            this.time = 1200 - Math.floor((this.now - this.start) / 1000);
-            this.testTimer = Math.floor(this.time / 60) + ":" + (this.time % 60);
-            this.endTimer = this.start + 1200000;
-            sessionStorage.setItem('recupEndTimer', this.endTimer);
-            if (this.now > this.endTimer) {
-                this.annulationResa();
-                this.resaTimeOut();
-            }
-            console.log(this.testTimer);
-            this.divTimer.innerHTML = "Temps restant :" + "<br/>" + this.testTimer;
-        }, 1000);
-
-    }
-    resaCheck() {
-        this.start = (new Date()).getTime();
-        this.divTimer = document.getElementById("recap_timer");
-        if (sessionStorage.getItem("start")) {
-            this.recupStation = document.getElementById('nom_station').textContent;
-            this.recapTexte = document.getElementById('recap_texte');
-            this.recapPrenom = localStorage.getItem('inputFirstName');
-            this.recapNom = localStorage.getItem('inputName');
-            this.sessionStation = sessionStorage.getItem("sessionStation");
-            this.recapTexte.innerText = " Vous avez reservé un vélo a la Station : " + this.sessionStation + " " + "Au nom de :" + " " + this.recapPrenom + " " + this.recapNom;
-            this.annulationBouton.style.display = "block";
-            this.start = sessionStorage.getItem("start");
-            console.log(this.start);
-            this.intervalTimer = setInterval(() => {
-                this.now = (new Date()).getTime();
-                // console.log(now-start);
-                this.time = 1200 - Math.floor((this.now - this.start) / 1000);
-                this.testTimer = Math.floor(this.time / 60) + ":" + (this.time % 60);
-                this.endTimer = sessionStorage.getItem('recupEndTimer');
-                console.log(this.now);
-                console.log(this.endTimer);
-                if (this.now > this.endTimer) {
-                    this.annulationResa();
-                    this.resaTimeOut();
-                }
-                console.log(this.testTimer);
-                this.divTimer.innerHTML = "Temps restant :" + "<br/>" + this.testTimer;
-            }, 1000);
-        }
-
     }
 
-    resaTimeOut() {
-        alert("Votre réservation a expiré");
-        this.divTimer.innerText = "";
-        this.recapText.innerText = " Votre reservation a expiré.";
-    }
-
-    annulationResa() {
-        clearInterval(this.intervalTimer);
-        sessionStorage.removeItem("start");
-        this.divTimer.innerText = " ";
-        this.recapText.innerText = "Vous n'avez pas de réservation pour le moment.";
-        this.annulationBouton.style.display = "none";
-
-
-    }
 }
 
 const classResa = new Reservation();
